88046e1ff3a0a834e4e7a6723bda1a8e2647cea5
wangzy <wangzy.vip@outlook.com>
2023-11-12 16:39:54 +0800

    feat: update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
+working_dir=$(cd $(dirname $0); pwd)
+echo "working dir: $working_dir"
+cd $working_dir
+if test -e api-go-darwin-arm64 ; then
+  rm api-go*
+fi
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o api-go-linux main.go
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o  api-go-windows main.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o api-go-darwin-arm64 main.go
--- common/console-args.go
+++ common/console-args.go
@@ -1,5 +1,12 @@
 package common
 
+// ConsoleArgs 命令行参数
+// M 请求方式
+// U 请求地址
+// P 请求参数
+// H 请求头
+// T 请求超时
+// HELP 帮助
 type ConsoleArgs struct {
 	M    string
 	U    string
