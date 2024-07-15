aefc28ef8aed5340ea51ed478504186f9003e827
wangzy <wangzy.vip@outlook.com>
2024-07-15 17:00:31 +0800

    feat: modify ignore file, and build script file

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ working_dir=$(cd $(dirname $0); pwd)
 echo "working dir: $working_dir"
 cd $working_dir
 if test -e api-go-darwin-arm64 ; then
-  rm api-go*
+  rm api-*
 fi
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o api-go-linux main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o  api-go-windows main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o api-go-darwin-arm64 main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o api-go-darwin-amd64 main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o api-linux main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o  api-windows main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o api-darwin-arm64 main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o api-darwin-amd64 main.go
