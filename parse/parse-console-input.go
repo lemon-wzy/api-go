75ddec0c360820fd2475c0f35bca38050b042bce
wangzy <wangzy.vip@outlook.com>
2024-07-15 17:13:28 +0800

    feat: modify parsefile

--- parse/parse-console-input.go
+++ parse/parse-console-input.go
@@ -14,11 +14,11 @@ var flagSet = flag.NewFlagSet("custom flag", flag.ExitOnError)
 // 初始化解析命令行参数
 func init() {
 	flagSet.StringVar(&consoleArgs.U, "u", "", "request url ")
-	flagSet.StringVar(&consoleArgs.M, "m", "GET", "request method ")
-	flagSet.Int64Var(&consoleArgs.T, "t", 0, "request timeout")
-	flagSet.Var(NewHeaderValue(map[string]string{}, &consoleArgs.H), "H", "request head")
+	flagSet.StringVar(&consoleArgs.M, "m", "GET", "request method GET or POST")
+	flagSet.Int64Var(&consoleArgs.T, "t", 9, "request timeout unit second")
+	flagSet.Var(NewHeaderValue(map[string]string{}, &consoleArgs.H), "H", "request header")
 	flagSet.Var(NewParamValue(map[string]any{}, &consoleArgs.P), "p", "request params")
-	flagSet.BoolVar(&consoleArgs.HELP, "h", false, "help")
+	flagSet.BoolVar(&consoleArgs.HELP, "h", false, "command help")
 	err := flagSet.Parse(os.Args[1:])
 	if err != nil {
 		os.Exit(1)
@@ -27,12 +27,12 @@ func init() {
 
 // Init 初始化
 func Init() {
-	if consoleArgs.HELP {
+	if consoleArgs.HELP || consoleArgs.U == "" {
 		flagSet.Usage()
 		os.Exit(0)
 	}
 	var result any
-	if consoleArgs.T != 0 {
+	if consoleArgs.T != 9 {
 		result = http_request.DoRequestByConsoleArgs(consoleArgs, http_request.CustomTimeOutClient(consoleArgs.T))
 	} else {
 		//call request and print response
